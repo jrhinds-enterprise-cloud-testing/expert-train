9a16f74f8d4da16f80e5c9c55d109c14798877ba
Jonathan R. Hinds <98826425+jrhinds@users.noreply.github.com>
2024-10-16 07:45:04 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,15 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "MyProject",
-    dependencies: [
-        .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.3"),
-        // Add other dependencies here
-    ],
     targets: [
         .target(
             name: "MyProject",
-            dependencies: ["CollectionConcurrencyKit", "CryptoSwift"]
+            dependencies: []
+        ),
+        .testTarget(
+            name: "MyProjectTests",
+            dependencies: ["MyProject"]
         ),
     ]
 )
