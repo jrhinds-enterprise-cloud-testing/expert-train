664f65d1163a7bbc46f58a635e4deacaf41ce420
Jonathan R. Hinds <98826425+jrhinds@users.noreply.github.com>
2024-10-16 08:26:57 -0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MyProject",
+    targets: [
+        .target(
+            name: "MyProject",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "MyProjectTests",
+            dependencies: ["MyProject"]
+        ),
+    ]
+)
